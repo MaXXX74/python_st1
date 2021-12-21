0eee01cb425fc2bd5fbded235f83c2c0d1fd4dad
MaXXX74 <ragalevich@yandex.ru>
2021-12-22 00:03:42 +0300

    Solved Task 2.6.9

--- main.py
+++ main.py
@@ -1,8 +1,15 @@
-lst = [int(i) for i in input().split()]
-find_num = int(input())
-if find_num not in lst:
-    print("Отсутствует")
-else:
-    for i in range(len(lst)):
-        if find_num == lst[i]:
-            print(i, end=" ")
+# вводим первоначальные значения
+matrix = []
+while True:
+    s = input()
+    if s != "end":
+        matrix.append([int(i) for i in s.split()])
+    else:
+        break
+# расчет и вывод
+rows = len(matrix)
+cols = len(matrix[0])
+for x in range(rows):
+    for y in range(cols):
+        print(matrix[x-1][y] + matrix[(x+1) % rows][y] + matrix[x][y-1] + matrix[x][(y+1) % cols], end=" ")
+    print()
